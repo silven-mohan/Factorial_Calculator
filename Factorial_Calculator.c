dbb7441ca7753366767e4db0a8c3206a12222da2
Silven Mohan <silven667@gmail.com>
2025-07-06 10:26:41 +0530

    Update Factorial_Calculator.c

--- Factorial_Calculator.c
+++ Factorial_Calculator.c
@@ -1,17 +1,19 @@
 /** Factorial Calculator **/
 
 #include<stdlib.h>
+#include<stdio.h>
 
-main()
+int main()
 {
 	int f=1, n, i;
-	system("cls");
+	// system("cls");
 	printf("Enter any number:");
 	scanf("%d", &n);
-	system("cls");
+	// system("cls");
 	for(i=n;i>=1;i--)
 	{
 		f=f*i;
 	}
 	printf("Factorial of %d is: %d",n, f);
+	return 0;
 }
